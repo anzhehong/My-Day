5eb6e1845da0963632237c7e71066b6cc07a7c90
anzhehong <anzhehong@126.com>
2015-06-16 10:47:36 +0800

    a

---
+++ My Day/BillModel.swift
@@ -0,0 +1,22 @@
+//
+//  BillModel.swift
+//  My Day
+//
+//  Created by FOWAFOLO on 15/6/16.
+//  Copyright (c) 2015 Fowafolo. All rights reserved.
+//
+
+import UIKit
+
+class BillModel: NSObject, NSCoding{
+    
+    
+    
+   
+    func encodeWithCoder(aCoder: NSCoder){
+        
+    }
+    required init(coder aDecoder: NSCoder) {
+        
+    }
+}
